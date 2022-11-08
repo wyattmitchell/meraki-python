579662b3d8214fbffae443dbccf52c0d6ee2dcc5
Mitchell Wyatt <mitwyatt@cisco.com>
2022-11-07 17:09:46 -0700

    Additional comments. Fix network search empty result (reprompt). Standardize variable naming.

--- migrate_all_aps.py
+++ migrate_all_aps.py
@@ -20,7 +20,7 @@ import os
 # ---- Begin Script Functions ----
 
 def select_org(dashboard):
-    # Fetch and select the organization
+    # Fetch, sort by name and select organization
     print('\n\nFetching organizations...\n')
     organizations = dashboard.organizations.getOrganizations()
     organizations.sort(key=lambda x: x['name'])
@@ -43,19 +43,30 @@ def select_org(dashboard):
     return(organizations[int(selected)]['id'], organizations[int(selected)]['name'])
 
 def select_net(dashboard, orgId):
-    # Fetch and select the network
+    # Fetch and select network
     networks = dashboard.organizations.getOrganizationNetworks(orgId)
 
     # Prompt search for network name to filter for shorter list.
-    search_name = input('Enter search string: ')
+    searchName = input('Enter search string or leave blank for all networks: ')
+
     netList = []
-    # Filter only MS devices
-    for net in networks:
-        if search_name in net["name"]:
-            netList.append(net)
+    while netList == []:
+        if searchName == '':
+            # Return all networks for blank search string.
+            netList = networks
+        else:   
+            # Return networks matching searchName.
+            for net in networks:
+                if searchName in net["name"]:
+                    netList.append(net)
+        # Validate at least 1 network is returned and prompt for new search string if empty.
+        if netList == []:
+            print(f'No networks found matching {searchName}.')
+            searchName = input('Enter search string or leave blank for all networks: ')
     
     netList.sort(key=lambda x: x['name'])
     counter = 0
+
     print('Networks:')
     for net in netList:
         netName = net['name']
@@ -92,6 +103,7 @@ def get_network_device_list(dashboard, netId, devString):
     return deviceList
 
 def select_device(deviceList):
+    # Sort and select from input devices list.
     deviceList.sort(key=lambda x: x['name'])
     counter = 0
     for dev in deviceList:
@@ -111,47 +123,48 @@ def select_device(deviceList):
     return(deviceList[int(selected)]['serial'], deviceList[int(selected)]['name'])
 
 def get_yn_response(question):
+    # Prompt for user Y/N response to input question.
     while True:
         resp = input(question).strip()
         if resp.lower() in ['y','n']:
             return resp.lower()
         print('Invalid response. Please enter Y or N.\n')
 
-def clear():
-     os.system('cls' if os.name=='nt' else 'clear')
-     return("   ")
 
-# ---- Begin Script Execution ----
+# ---- Begin Script Main ----
 
+# Debug options
+### Additional logging to Console
 consoleDebug = False
+### Log Meraki SDK activity to file
 logDebug = False
 
 # Connect to dashboard, select org and network
 dashboard = meraki.DashboardAPI(suppress_logging=not logDebug)
-selected_org, orgName = select_org(dashboard)
+orgId, orgName = select_org(dashboard)
 
 print('\n\nSelect source network...\n')
-src_net, src_netName = select_net(dashboard, selected_org)
+srcNetId, srcNetName = select_net(dashboard, orgId)
 
 print('\n\nSelect destination network...\n')
-dst_net, dst_netName = select_net(dashboard, selected_org)
+dstNetId, dstNetName = select_net(dashboard, orgId)
 
-confirm = get_yn_response(f'\nSource network {src_netName} and destination network {dst_netName} selected. Continue? (Y/N): ')
+confirm = get_yn_response(f'\nSource network {srcNetName} and destination network {dstNetName} selected. Continue? (Y/N): ')
 
 if confirm == 'n':
     exit()
 
 # Get list of network devices
 devString = 'MR'
-deviceList = get_network_device_list(dashboard, src_net, devString)
+deviceList = get_network_device_list(dashboard, srcNetId, devString)
 if deviceList == []:
     print('No matching devices found in network. Exiting...')
 
 if consoleDebug == True: print(deviceList)
 
 # Get source and destination floorplans.
-src_floorplans = dashboard.networks.getNetworkFloorPlans(src_net)
-dst_floorplans = dashboard.networks.getNetworkFloorPlans(dst_net)
+src_floorplans = dashboard.networks.getNetworkFloorPlans(srcNetId)
+dst_floorplans = dashboard.networks.getNetworkFloorPlans(dstNetId)
 
 for ap in deviceList:
     serial = ap['serial']
@@ -186,10 +199,10 @@ for ap in deviceList:
         print(f'No matching destination floor plan found for {name}. Continuing...')
 
     print(f'\nRemoving {name} from source network...')
-    dashboard.networks.removeNetworkDevices(src_net, serial)
+    dashboard.networks.removeNetworkDevices(srcNetId, serial)
     claimSerial = [serial]
     print(f'Adding {name} to destination network...')
-    dashboard.networks.claimNetworkDevices(dst_net, claimSerial)            
+    dashboard.networks.claimNetworkDevices(dstNetId, claimSerial)            
 
     if dstFound == True:
         try:
